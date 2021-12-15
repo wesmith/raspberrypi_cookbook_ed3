21a247cd41094367f97ebf79c5f3a06013736746
Warren Smith <smiwarsky@gmail.com>
2021-12-14 18:14:31 -0700

    added temp in deg C and deg F

--- python/ch_07_bottle_test.py
+++ python/ch_07_bottle_test.py
@@ -1,10 +1,22 @@
+# WS added CPU temp
+
 from bottle import route, run, template
 from datetime import datetime
+import os
+
+def get_temp(): # WS added
+    dev = os.popen('/opt/vc/bin/vcgencmd measure_temp')
+    cpu_temp_str = dev.read()[5:-3] # top and tail string
+    cpu_temp_C   = float(cpu_temp_str)
+    cpu_temp_F   = cpu_temp_C * 9/5 + 32.0
+    return cpu_temp_C, cpu_temp_F
 
 @route('/')
 def index(name='time'):
-    dt = datetime.now()
-    time = "{:%Y-%m-%d %H:%M:%S}".format(dt)
-    return template('<b>Pi thinks the date/time is: {{t}}</b>', t=time)
+    dt     = datetime.now()
+    time   = "{:%Y-%m-%d %H:%M:%S}".format(dt)
+    tc, tf = get_temp()
+    return template('<b>time: {{t}}, cpu temp: {{tc}} deg C, {{tf}} deg F</b>',
+                    t=time, tc=tc, tf=tf)
 
 run(host='0.0.0.0', port=80)
